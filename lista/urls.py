39675eaa9ded341cc1c36031321895151b06e2ba
shelshock360 <joaom4936@gmail.com>
2020-02-12 17:35:50 -0300

    cadastrando produtos

--- lista/urls.py
+++ lista/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', PaginaInicialView.as_view(), name="index"),
+    path('cadastrar/produto/', ProdutoCreate.as_view(), name="cadastrar-produto"),
     
 ]
  
--- lista/views.py
+++ lista/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import *
+from django.urls import reverse_lazy 
 
-
+# importaçao pra alterar excluir consultar
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+# Create your views here.
 
 class PaginaInicialView(TemplateView):
     template_name = "lista/index.html"
 
+
+class ProdutoCreate (CreateView):
+    model = Produto
+    fields = ['nome','preco','qtde']
+    template_name = 'lista/formulario.html'
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProdutoCreate, self).get_context_data(*args, **kwargs)
+
+        # adiciona coisas ao contextos das coisas
+        context['titulo'] = "Cadastro de novos produtos"
+        context['botao'] = "Cadastrar"
+        context['classbotao'] = "btn-success"
+
+        return context
     
 
-# Create your views here.
+
+
+
